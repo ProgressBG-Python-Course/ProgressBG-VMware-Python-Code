6854599f1e33151c775f7bfb7e9315597fe69564
ProgressBG-Python-Course <progressbg.python.course@gmail.com>
2019-01-28 13:05:36 +0200

    Lab4

---
+++ lab4/switch_case_emulation.py
@@ -0,0 +1,24 @@
+print("Select an action:")
+print("1. Action 1")
+print("2. Action 3")
+print("3. Action 3")
+
+user_choice = int(input('Enter a choice:'))
+
+
+if user_choice == 1:
+  print('Action 1 is triggered')
+elif user_choice == 2:
+  print('Action 2 is triggered')
+elif user_choice == 3:
+  print('Action 3 is triggered')
+
+
+
+# switch user_choice:
+#   case 1:
+#     print('Action 1 is triggered')
+#   case 2:
+#     print('Action 2 is triggered')
+#   case 3:
+#     print('Action 3 is triggered')
---
+++ lab4/tuples.py
@@ -0,0 +1,9 @@
+l = [1]
+print( type(l) )
+
+t = ()
+print( type(t) )
+
+
+address = ('Bulgaria', 'Sofia', 'Nezabravka str', 14)
+address = ('UK', 'Lond', 'Nezabravka str', 14)
