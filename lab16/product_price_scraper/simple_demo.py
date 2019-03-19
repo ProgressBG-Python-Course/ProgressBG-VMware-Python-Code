280904e8b6a00fb934a07d0589e065042a268f3a
ProgressBG-Python-Course <progressbg.python.course@gmail.com>
2019-03-20 00:07:12 +0200

    init: Lab16

---
+++ lab16/product_price_scraper/simple_demo.py
@@ -0,0 +1,60 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import re
+
+
+
+def get_html(url):
+	if url.startswith("http"):
+		# make the request - change UA to prevent server deny for scripts
+		req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+		# get response
+		respone = request.urlopen(req)
+		return respone.read()
+	else:
+		with open(url,"r") as f:
+		  return f.read()
+
+def scrape_data(page):
+	bs_parser = BeautifulSoup(page, 'html.parser')	
+
+	products_list = bs_parser.find('ul', class_='products')
+
+	for product in products_list.find_all("li"):	
+			try:				
+				tmp = product.select("a h2")	
+				name = tmp[0].string
+				price_str = str(product.find('span', class_='price'))			
+				# <span class="price">659<span class="currency">лв.</span></span>
+				res = re.search(r'<span class="price">(\d+)', price_str)
+				price = res.groups()[0]
+				
+			except:
+				continue
+
+	
+	products.append((name,int(price)))
+
+def print_scraped_data(products):
+	sorted_by_price = sorted(products, key=lambda p: p[1])
+	for i in sorted_by_price:
+		print("{} - {}".format(i[0],i[1]))
+
+
+start_url = "https://laptop.bg/laptops-lenovo"
+pages = 1
+products = []
+
+for i in range(1,pages+1):
+	# https://laptop.bg/laptops-lenovo?page=2
+	url = start_url+f'?page={i}'
+	print("Scraping URL:", url)
+	page_content = get_html(url)
+	scrape_data(page_content)
+
+	
+print_scraped_data(products)
+
+
+
---
+++ lab16/pythonbooks_revolunet_scraper/pythonbooks.py
@@ -0,0 +1,34 @@
+from urllib import request
+import json
+
+
+def get_html(url):
+	""" Summary
+	
+			Args:
+			    url (string): a URL to get content from 
+			
+			Returns:
+			    string: the content of URL
+	"""
+	
+	# make the request - change UA to prevent server deny for scripts
+	req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+	# get response
+	respone = request.urlopen(req)
+	
+	return respone.read()
+	
+
+if __name__ == '__main__':	
+	url = "https://raw.githubusercontent.com/revolunet/PythonBooks/master/issues.json"	
+
+	page_content = get_html(url)
+
+	books = json.loads(page_content)
+	
+	print(books['books'][0])
+
+
+
